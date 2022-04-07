1e9ec807d835c7ce8c5f4965c9c6418ccd7b46da
Angelita Santos <angelitasantos@gmail.com>
2022-04-07 16:01:05 -0300

    alter style python flask

---
+++ README.md
@@ -0,0 +1,14 @@
+## Site criado com Python / Flask
+
+
+### Javascript
+
+[onclick event](https://www.w3schools.com/jsref/event_onclick.asp)
+
+
+#### Funções
+
+- onclick="desabilitado()"
+- somar valores
+
+
---
+++ static/js/script.js
@@ -0,0 +1,30 @@
+
+var calculadora = "Calculadora"
+var simples = "Simples"
+var cientifica = "Científica"
+var financeira = "Financeira"
+
+document.getElementById("calculadora").innerHTML = calculadora;
+document.getElementById("simples").innerHTML = simples;
+document.getElementById("cientifica").innerHTML = cientifica;
+document.getElementById("financeira").innerHTML = financeira;
+
+var valor;
+var resultado;
+
+function botao(numero) {
+    valor = document.calc_simples.visor_simples.value += numero;
+}
+
+function limpar() {
+    document.calc_simples.visor_simples.value = '';
+}
+
+function calcular() {
+    resultado = eval(valor)
+    document.calc_simples.visor_simples.value = resultado;
+}
+
+function desabilitado() {
+    alert('Função Desabilitada');
+}
